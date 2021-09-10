55a0734860a714abc73ed27e62e542e949ceb542
Chris <chris.ware.dev@gmail.com>
2021-09-09 22:46:36 -0700

    About/Events/Slug pages

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,10 @@ function AboutPage() {
   return (
     <div>
       <h1>About</h1>
+      <p>
+        This is an app to help you find the latest DJ and other musical events.
+      </p>
+      <p>Version 1.0.0</p>
     </div>
   );
 }
