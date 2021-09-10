ba72ec58907bf6fc35cf7139901191834ce6c80d
Chris <chris.ware.dev@gmail.com>
2021-09-10 10:29:55 -0700

    Footer Router Adjustment

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,16 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from '../styles/Footer.module.css'
+import styles from "../styles/Footer.module.css";
 
 function Footer() {
+  const router = useRouter();
+
   return (
     <footer className={styles.footer}>
       <p>Copyright &copy; DJ Events 2021</p>
       <p>
-        <Link href='/about'>About This project</Link>
+        {router.pathname !== "/about" && <Link href='/about'>About This project</Link>}
+        
       </p>
     </footer>
   );
