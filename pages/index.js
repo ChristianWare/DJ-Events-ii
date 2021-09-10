725888483318c71bffc027c2b24012fee7b7ca64
Chris <chris.ware.dev@gmail.com>
2021-09-09 23:42:37 -0700

    Head and Tag Layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
   );
 }
